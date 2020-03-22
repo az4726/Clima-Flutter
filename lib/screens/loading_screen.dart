6b86a0a42b419173d6ca2e139500879cc875e466
Azhar Zaman <azhar.zaman99@gmail.com>
2020-03-22 13:01:41 +0000

    Use sample weather data to extract required information from JSON output

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -18,8 +20,31 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+
+      var decodedData = jsonDecode(data);
+
+      double latitude = decodedData['coord']['lat'];
+      print(latitude);
+
+      String weatherHeader = decodedData['weather'][0]['main'];
+      print(weatherHeader);
+
+      double temperatur = decodedData['main']['temp'];
+      print(temperatur);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
